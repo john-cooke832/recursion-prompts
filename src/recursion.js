07c83e28c8b91e5b3a97d5ef7280c2d8822443ef
John Cooke <johncooke832@gmail.com>
2020-12-20 11:31:03 -0600

    Implemented the sum of an array

--- src/recursion.js
+++ src/recursion.js
@@ -16,6 +16,12 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  let length = array.length;
+  if(length === 0){
+    return 0;
+  }
+  length--;
+  return array[length] + sum(array.slice(0, length));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
